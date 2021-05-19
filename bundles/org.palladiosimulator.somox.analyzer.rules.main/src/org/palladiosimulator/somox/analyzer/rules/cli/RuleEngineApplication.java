f47f9e2ba6a3a334ed6191262551721a350294f5
FloBoJa <floboja@t-online.de>
2021-05-19 12:03:08 +0200

    Check command-line arguments and provide feedback

--- bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/cli/RuleEngineApplication.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/cli/RuleEngineApplication.java
@@ -1,8 +1,10 @@
 package org.palladiosimulator.somox.analyzer.rules.cli;
 
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
 import org.eclipse.equinox.app.IApplication;
@@ -13,22 +15,54 @@ import org.palladiosimulator.somox.analyzer.rules.engine.ParserAdapter;
 import org.palladiosimulator.somox.analyzer.rules.main.RuleEngineAnalyzer;
 
 public class RuleEngineApplication implements IApplication {
+    
+    private static final String FORMAT_EXPLANATION = "The following format is expected:"
+            + "\n<input directory> <output directory> [rules]\n\nSupported rules: "
+            + String.join(", ", DefaultRule.valuesAsString());
 
     @Override
     public Object start(IApplicationContext context) throws Exception {
         String[] args = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-        final Path in = Paths.get(args[0]);
-        final Path out = Paths.get(args[1]);
+        
+        if (args.length < 2) {
+            System.err.println("Too few arguments!\n" + FORMAT_EXPLANATION);
+        }
+        
+        // Extract and check path arguments
+        final Path in;
+        try {
+            in = Paths.get(args[0]);
+        } catch (InvalidPathException e) {
+            System.err.println("Invalid path: \"" + args[0] + "\"\n" + FORMAT_EXPLANATION);
+            return -1;
+        }
+        final Path out;
+        try {
+            out = Paths.get(args[1]);
+        } catch (InvalidPathException e) {
+            System.err.println("Invalid path: \"" + args[1] + "\"\n" + FORMAT_EXPLANATION);
+            return -1;
+        }
+        
+        // Extract and check rules
+        final Set<DefaultRule> rules = new HashSet<DefaultRule>();
+        for (int i = 2; i < args.length; i++) {
+            try {
+                rules.add(DefaultRule.valueOf(args[i]));
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid rule: \"" + args[i] + "\"\n" + FORMAT_EXPLANATION);
+                return -1;
+            }
+        }
+        
         final List<CompilationUnitImpl> roots = ParserAdapter.generateModelForProject(in);
-        final Set<DefaultRule> rules = Set.of(DefaultRule.values());
         
         RuleEngineAnalyzer.executeWith(in, out, roots, rules);
         
-        return null;
+        return 0;
     }
 
     @Override
     public void stop() {
     }
-
 }
