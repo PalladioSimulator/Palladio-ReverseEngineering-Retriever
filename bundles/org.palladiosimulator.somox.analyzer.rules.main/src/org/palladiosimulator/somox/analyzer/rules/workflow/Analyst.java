191073db433b60d9c857a7108a20e996789767ff
Florian Bossert <ufngx@student.kit.edu>
2021-12-21 10:12:56 +0100

    Define Analyst

--- bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/Analyst.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/Analyst.java
@@ -1,5 +1,17 @@
 package org.palladiosimulator.somox.analyzer.rules.workflow;
 
-public interface Analyst {
+import org.palladiosimulator.somox.analyzer.rules.blackboard.RuleEngineBlackboard;
+import org.palladiosimulator.somox.analyzer.rules.configuration.RuleEngineConfiguration;
 
+import de.uka.ipd.sdq.workflow.jobs.AbstractJob;
+
+/**
+ * The defining interface of the org.palladiosimulator.somox.analyzer.rules.analyst extension point.
+ * Implement this interface to extend the rule engine by an additional analyst that can then process
+ * the generated model.
+ * 
+ * @author Florian Bossert
+ */
+public interface Analyst {
+    AbstractJob create(RuleEngineConfiguration configuration, RuleEngineBlackboard blackboard);
 }
