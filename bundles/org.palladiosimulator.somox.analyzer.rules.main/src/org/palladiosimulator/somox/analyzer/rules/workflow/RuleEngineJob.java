80d1ada8e05b33d36319e01f7a7de021d5cae33e
Florian Bossert <ufngx@student.kit.edu>
2021-12-21 10:44:12 +0100

    Create jobs for extending analyzers

--- bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/RuleEngineJob.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/RuleEngineJob.java
@@ -1,42 +1,51 @@
 package org.palladiosimulator.somox.analyzer.rules.workflow;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.palladiosimulator.somox.analyzer.rules.blackboard.RuleEngineBlackboard;
 import org.palladiosimulator.somox.analyzer.rules.configuration.RuleEngineAnalyzerConfiguration;
+import org.palladiosimulator.somox.analyzer.rules.configuration.RuleEngineConfiguration;
 
 import de.uka.ipd.sdq.workflow.extension.AbstractExtendableJob;
 import de.uka.ipd.sdq.workflow.jobs.ParallelJob;
 
 public class RuleEngineJob extends AbstractExtendableJob<RuleEngineBlackboard> {
 
-    public RuleEngineJob(RuleEngineAnalyzerConfiguration configuration) {
+    private static final String ANALYST_EXTENSION_POINT = "org.palladiosimulator.somox.analyzer.rules.analyst";
+
+    public RuleEngineJob(RuleEngineAnalyzerConfiguration configuration) throws CoreException {
         setBlackboard(new RuleEngineBlackboard());
 
         this.add(discoveryJobs(configuration));
-        
-        this.add(new RuleEngineBlackboardInteractingJob(configuration, getBlackboard()));
+
+        this.add(new RuleEngineBlackboardInteractingJob(configuration.getMoxConfiguration(), getBlackboard()));
 
         // TODO integration SEFF extraction
         // this.add(new SeffCreatorJob(false, null, null));
 
         this.add(new ModelSaverJob(configuration.getMoxConfiguration()));
-        
-        /* TODO iterate through extension point implementations
-        if (configuration.getMoxConfiguration().isSnykEnabled()) {
-            this.add(new SnykCodeAnalystJob(configuration.getMoxConfiguration(), getBlackboard()));
-        }
-        */
+
+        addAnalysts(configuration.getMoxConfiguration());
     }
 
-    
-    
     private ParallelJob discoveryJobs(RuleEngineAnalyzerConfiguration configuration) {
-    	ParallelJob discoveryJobs = new ParallelJob();
-    	
-    	discoveryJobs.add(new JdtParserJob(configuration, getBlackboard()));
-    	// TODO Add Job for Docker discovery
-    	
-		return discoveryJobs;
-    }
+        ParallelJob discoveryJobs = new ParallelJob();
 
+        discoveryJobs.add(new JdtParserJob(configuration, getBlackboard()));
+        // TODO Add Job for Docker discovery
+
+        return discoveryJobs;
+    }
     
+    private void addAnalysts(RuleEngineConfiguration configuration) throws CoreException {
+        for (IConfigurationElement extension : Platform.getExtensionRegistry()
+            .getConfigurationElementsFor(ANALYST_EXTENSION_POINT)) {
+            final Object o = extension.createExecutableExtension("class");
+            if (o instanceof Analyst) {
+                logger.info(String.format("Adding analyst %s", extension.getDeclaringExtension().getUniqueIdentifier()));
+                this.add(((Analyst) o).create(configuration, getBlackboard()));
+            }
+        }
+    }
 }
