ffbdaec6c105dfe761fe4b9aa9c09f828ac3ed40
Florian Bossert <ufngx@student.kit.edu>
2022-04-25 17:38:55 +0200

    Use static test repository versions

--- tests/org.palladiosimulator.somox.analyzer.rules.engine.test/src/org/palladiosimulator/somox/analyzer/rules/engine/test/PiggymetricsTest.java
+++ tests/org.palladiosimulator.somox.analyzer.rules.engine.test/src/org/palladiosimulator/somox/analyzer/rules/engine/test/PiggymetricsTest.java
@@ -6,7 +6,7 @@ import org.palladiosimulator.somox.analyzer.rules.all.DefaultRule;
 public class PiggymetricsTest extends RuleEngineTest {
 
     protected PiggymetricsTest() {
-        super("external/piggymetrics-master", DefaultRule.SPRING);
+        super("external/piggymetrics-spring.version.2.0.3", DefaultRule.SPRING);
     }
 
     /**
