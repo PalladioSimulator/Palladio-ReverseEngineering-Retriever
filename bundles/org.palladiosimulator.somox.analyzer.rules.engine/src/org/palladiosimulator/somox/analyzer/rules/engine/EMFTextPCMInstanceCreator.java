3c978863ba3e64232f9f30ec52ca77798dc307d8
Florian Bossert <ufngx@student.kit.edu>
2022-06-28 09:55:39 +0200

    Fix or remove warnings in EMFTextPCMInstanceCreator.java

--- bundles/org.palladiosimulator.somox.analyzer.rules.engine/src/org/palladiosimulator/somox/analyzer/rules/engine/EMFTextPCMInstanceCreator.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.engine/src/org/palladiosimulator/somox/analyzer/rules/engine/EMFTextPCMInstanceCreator.java
@@ -16,7 +16,6 @@ import org.emftext.language.java.classifiers.Interface;
 import org.emftext.language.java.containers.impl.CompilationUnitImpl;
 import org.emftext.language.java.generics.QualifiedTypeArgument;
 import org.emftext.language.java.generics.TypeArgument;
-import org.emftext.language.java.members.Field;
 import org.emftext.language.java.members.Method;
 import org.emftext.language.java.parameters.Parameter;
 import org.emftext.language.java.parameters.impl.OrdinaryParameterImpl;
@@ -108,8 +107,6 @@ public class EMFTextPCMInstanceCreator {
 
                 // parameter type
                 for (final Parameter p : m.getParameters()) {
-
-                    final TypeReference ref = p.getTypeReference();
                     signature = handleSignatureDataType(signature, p.getClass(), p.getName(), p.getTypeReference(),
                             p.getArrayDimensionsBefore(), false);
                 }
@@ -156,14 +153,6 @@ public class EMFTextPCMInstanceCreator {
             blackboard.putRepositoryComponentLocation(builtComp, new CompilationUnitWrapper(comp));
             repository.addToRepository(builtComp);
         }
-    }
-
-    private static String getProvidesName(String compName, String opName) {
-        return (compName + " provides " + opName);
-    }
-
-    private static String getRequiresName(String compName, String opName) {
-        return (compName + " requires " + opName);
     }
 
     private static String getCompName(CompilationUnitImpl comp) {
@@ -317,6 +306,7 @@ public class EMFTextPCMInstanceCreator {
         return collectionType;
     }
 
+    @SuppressWarnings("static-access")
     private CollectionDataType createCollectionWithTypeArg(String collectionTypeName, TypeReference typeArg,
             List<ArrayDimension> typeArgDimensions) {
         // Type argument is primitive
@@ -392,47 +382,12 @@ public class EMFTextPCMInstanceCreator {
         String classifierName = classifier.getName();
 
         if (!existingDataTypesMap.containsKey(classifierName)) {
-            // existingDataTypesMap.put(type.getName(), createTypesRecursively(type));
             existingDataTypesMap.put(classifierName, create.newCompositeDataType()
                 .withName(classifierName));
             repository.addToRepository(existingDataTypesMap.get(classifierName));
         }
 
         return create.fetchOfCompositeDataType(classifierName);
-    }
-
-    // TODO creation of CompositeDataTypes
-    private CompositeDataTypeCreator createTypesRecursively(ConcreteClassifier type) {
-        if (existingDataTypesMap.containsKey(type.getName())) {
-            return existingDataTypesMap.get(type.getName());
-        }
-
-        CompositeDataTypeCreator currentDataType = create.newCompositeDataType()
-            .withName(type.getName());
-        for (Field f : type.getFields()) {
-
-            if (f.getTypeReference() instanceof PrimitiveType) {
-                currentDataType = currentDataType.withInnerDeclaration(f.getName(),
-                        convertPrimitive((PrimitiveType) f.getTypeReference()));
-            } else if (f.getTypeReference()
-                .getTarget()
-                .toString()
-                .equals("String")) {
-                currentDataType = currentDataType.withInnerDeclaration(f.getName(), Primitive.STRING);
-            } else if (f.getTypeReference()
-                .getTarget()
-                .toString()
-                .equals("List")) {
-                currentDataType = currentDataType.withInnerDeclaration(f.getName(),
-                        create.newCollectionDataType(f.getName(), Primitive.BYTE));
-            } else {
-                currentDataType = currentDataType.withInnerDeclaration(f.getName(),
-                        createTypesRecursively(getConcreteFromVar(f)).build());
-            }
-        }
-
-        repository.addToRepository(currentDataType);
-        return currentDataType;
     }
 
 }
