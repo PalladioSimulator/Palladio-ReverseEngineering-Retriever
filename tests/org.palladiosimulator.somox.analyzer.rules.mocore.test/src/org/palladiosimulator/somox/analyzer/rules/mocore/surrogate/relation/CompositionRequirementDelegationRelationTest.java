7e1c14d0a31cbdd01c9abf6aa48b9c749b9805d6
Moritz Gstuer <moritz@gstuer.com>
2023-02-19 21:21:09 +0100

    Add req. deleg. relation constructor guard tests

--- tests/org.palladiosimulator.somox.analyzer.rules.mocore.test/src/org/palladiosimulator/somox/analyzer/rules/mocore/surrogate/relation/CompositionRequirementDelegationRelationTest.java
+++ tests/org.palladiosimulator.somox.analyzer.rules.mocore.test/src/org/palladiosimulator/somox/analyzer/rules/mocore/surrogate/relation/CompositionRequirementDelegationRelationTest.java
@@ -1,5 +1,8 @@
 package org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.relation;
 
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.element.AtomicComponent;
 import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.element.Component;
 import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.element.Composite;
@@ -10,6 +13,39 @@ import tools.mdsd.mocore.framework.surrogate.RelationTest;
 public class CompositionRequirementDelegationRelationTest extends RelationTest<CompositeRequirementDelegationRelation,
         InterfaceRequirementRelation, InterfaceRequirementRelation> {
     private static final Interface RELATION_INTERFACE = Interface.getUniquePlaceholder();
+
+    @Test
+    public void testConstructorWithEqualSourceAndDestinationRelation() {
+        InterfaceRequirementRelation entity = getUniqueSourceEntity();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new CompositeRequirementDelegationRelation(entity, entity, false));
+    }
+
+    @Test
+    public void testConstructorWithoutSourceCompositeComponent() {
+        AtomicComponent sourceComponent = AtomicComponent.getUniquePlaceholder();
+        Composite destinationComponent = Composite.getUniquePlaceholder();
+        InterfaceRequirementRelation source = new InterfaceRequirementRelation(sourceComponent,
+                RELATION_INTERFACE, true);
+        InterfaceRequirementRelation destination = new InterfaceRequirementRelation(destinationComponent,
+                RELATION_INTERFACE, true);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new CompositeRequirementDelegationRelation(source, destination, false));
+    }
+
+    @Test
+    public void testConstructorWithDifferentRequirementInterfaces() {
+        InterfaceRequirementRelation source = getUniqueSourceEntity();
+        Component<?> destinationComponent = AtomicComponent.getUniquePlaceholder();
+        Interface destinationInterface = Interface.getUniquePlaceholder();
+        InterfaceRequirementRelation destination = new InterfaceRequirementRelation(destinationComponent,
+                destinationInterface, true);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new CompositeRequirementDelegationRelation(source, destination, false));
+    }
 
     @Override
     protected CompositeRequirementDelegationRelation createRelation(InterfaceRequirementRelation source,
