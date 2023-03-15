afa9b3017170f8834384206bfc72ed6566959121
Florian Bossert <ufngx@student.kit.edu>
2023-03-15 10:52:13 +0100

    Add more specific test case to SeffAssociationTest

--- tests/org.palladiosimulator.somox.analyzer.rules.test/src/org/palladiosimulator/somox/analyzer/rules/test/integration/SeffAssociationTest.java
+++ tests/org.palladiosimulator.somox.analyzer.rules.test/src/org/palladiosimulator/somox/analyzer/rules/test/integration/SeffAssociationTest.java
@@ -1,14 +1,15 @@
 package org.palladiosimulator.somox.analyzer.rules.test.integration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.junit.jupiter.api.Test;
 import org.palladiosimulator.pcm.seff.ServiceEffectSpecification;
 import org.palladiosimulator.somox.analyzer.rules.all.DefaultRule;
@@ -18,23 +19,19 @@ import org.palladiosimulator.somox.analyzer.rules.blackboard.RuleEngineBlackboar
 public class SeffAssociationTest extends RuleEngineTest {
 
     SeffAssociationTest() {
-        super("external/piggymetrics-spring.version.2.0.3", DefaultRule.SPRING);
+        super("SpringProject", DefaultRule.SPRING);
     }
 
     @Test
-    void test() {
+    void allAssociationsReferToExistingMethods() {
         RuleEngineBlackboard blackboard = getBlackboard();
         Map<ASTNode, ServiceEffectSpecification> associations = blackboard.getSeffAssociations();
-        Set<CompilationUnit> compilationUnits = blackboard.getCompilationUnits()
-            .stream()
-            .filter(CompilationUnitWrapper::isEclipseCompilationUnit)
-            .map(CompilationUnitWrapper::getEclipseCompilationUnit)
-            .collect(Collectors.toSet());
 
         for (Map.Entry<ASTNode, ServiceEffectSpecification> association : associations.entrySet()) {
             ASTNode astNode = association.getKey();
             ServiceEffectSpecification seff = association.getValue();
-            assertTrue(astNode instanceof MethodDeclaration);
+            assertTrue(astNode instanceof MethodDeclaration,
+                    "All ASTNodes in the SEFF/AST associations must be MethodDeclarations");
 
             MethodDeclaration methodDeclaration = (MethodDeclaration) astNode;
             String declarationName = methodDeclaration.getName()
@@ -44,7 +41,37 @@ public class SeffAssociationTest extends RuleEngineTest {
             // SEFF names may have a "$N" suffix after the signature name, where N is a positive
             // integer.
             // This avoids name collisions that can occur because signature identifiers are global.
-            assertTrue(seffName.startsWith(declarationName));
+            assertTrue(seffName.startsWith(declarationName), "SEFF's name must begin with its method's name");
         }
+    }
+
+    @Override
+    @Test
+    void test() {
+        RuleEngineBlackboard blackboard = getBlackboard();
+
+        @SuppressWarnings("unchecked")
+        MethodDeclaration constructorDeclaration = blackboard.getCompilationUnits()
+            .stream()
+            .filter(CompilationUnitWrapper::isEclipseCompilationUnit)
+            .map(CompilationUnitWrapper::getEclipseCompilationUnit)
+            .flatMap(unit -> ((List<ASTNode>) unit.types()).stream())
+            .filter(TypeDeclaration.class::isInstance)
+            .map(TypeDeclaration.class::cast)
+            .filter(type -> type.getName()
+                .getFullyQualifiedName()
+                .equals("AComponent"))
+            .flatMap(type -> Stream.of(type.getMethods()))
+            .filter(method -> method.getName()
+                .getFullyQualifiedName()
+                .equals("AComponent"))
+            .findAny()
+            .orElseGet(() -> fail("Constructor of AComponent must be present in AST"));
+
+        Map<ASTNode, ServiceEffectSpecification> associations = blackboard.getSeffAssociations();
+
+        // Assume that the SPRING rule detects constructors of components.
+        assertTrue(associations.containsKey(constructorDeclaration),
+                "SEFF/AST associations must contain all rule-detected methods");
     }
 }
