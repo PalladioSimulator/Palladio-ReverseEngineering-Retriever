0c9b7d82ff6c0e9f36312ab07f7f5b87ada6f125
Moritz Gstuer <moritz@gstuer.com>
2023-05-31 16:36:36 +0200

    Add refine rule for implicit component assembly

--- bundles/org.palladiosimulator.somox.analyzer.rules.mocore/src/org/palladiosimulator/somox/analyzer/rules/mocore/processor/relation/InterfaceRequirementRelationProcessor.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.mocore/src/org/palladiosimulator/somox/analyzer/rules/mocore/processor/relation/InterfaceRequirementRelationProcessor.java
@@ -1,12 +1,37 @@
 package org.palladiosimulator.somox.analyzer.rules.mocore.processor.relation;
 
+import java.util.List;
+
 import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.PcmSurrogate;
+import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.element.Interface;
+import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.relation.ComponentAssemblyRelation;
+import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.relation.InterfaceProvisionRelation;
 import org.palladiosimulator.somox.analyzer.rules.mocore.surrogate.relation.InterfaceRequirementRelation;
 
 import tools.mdsd.mocore.framework.processor.RelationProcessor;
 
-public class InterfaceRequirementRelationProcessor extends RelationProcessor<PcmSurrogate, InterfaceRequirementRelation> {
+public class InterfaceRequirementRelationProcessor
+        extends RelationProcessor<PcmSurrogate, InterfaceRequirementRelation> {
     public InterfaceRequirementRelationProcessor(PcmSurrogate model) {
         super(model, InterfaceRequirementRelation.class);
+    }
+
+    @Override
+    protected void refine(InterfaceRequirementRelation discovery) {
+        Interface commonInterface = discovery.getDestination();
+
+        // Get all requirements from model & filter for same interface as in discovery
+        List<InterfaceProvisionRelation> provisionRelations = this.getModel()
+                .getByType(InterfaceProvisionRelation.class);
+        provisionRelations.removeIf(relation -> !relation.getDestination().equals(commonInterface));
+
+        // Create component assembly placeholder for pairs of provision & requirement relations
+        for (InterfaceProvisionRelation provisionRelation : provisionRelations) {
+            ComponentAssemblyRelation assemblyRelation = new ComponentAssemblyRelation(provisionRelation,
+                    discovery, true);
+            this.addImplication(assemblyRelation);
+        }
+
+        super.refine(discovery);
     }
 }
