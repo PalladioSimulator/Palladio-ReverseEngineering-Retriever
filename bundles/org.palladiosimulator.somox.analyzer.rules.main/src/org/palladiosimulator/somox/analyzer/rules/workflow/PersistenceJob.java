15ce1fef8b16120468350a47d4728a0ad254b17c
Florian Bossert <ufngx@student.kit.edu>
2023-07-02 11:19:41 +0200

    Support persisting to platform:/ URIs

--- bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/PersistenceJob.java
+++ bundles/org.palladiosimulator.somox.analyzer.rules.main/src/org/palladiosimulator/somox/analyzer/rules/workflow/PersistenceJob.java
@@ -1,9 +1,10 @@
 package org.palladiosimulator.somox.analyzer.rules.workflow;
 
-import java.io.File;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.util.URI;
 import org.palladiosimulator.generator.fluent.shared.util.ModelSaver;
 import org.palladiosimulator.pcm.allocation.Allocation;
@@ -25,18 +26,26 @@ public class PersistenceJob implements IBlackboardInteractingJob<Blackboard<Obje
     private final String systemKey;
     private final String allocationKey;
     private final String resourceEnvironmentKey;
-    private final File outputFilePrefix;
+    private final String outputPrefix;
 
     public PersistenceJob(Blackboard<Object> blackboard, URI inputFolder, URI outputFolder, String repositoryKey,
             String systemKey, String allocationKey, String resourceEnvironmentKey) {
         this.blackboard = Objects.requireNonNull(blackboard);
         String configuredInputProjectName = inputFolder.lastSegment();
+        // Handle a trailing path separator ("example/path/").
+        if (configuredInputProjectName.isEmpty()) {
+            configuredInputProjectName = inputFolder.trimSegments(1)
+                .lastSegment();
+        }
         this.repositoryKey = Objects.requireNonNull(repositoryKey);
         this.systemKey = Objects.requireNonNull(systemKey);
         this.allocationKey = Objects.requireNonNull(allocationKey);
         this.resourceEnvironmentKey = Objects.requireNonNull(resourceEnvironmentKey);
         // Set path to output folder and prefix of all output files to name of input project folder
-        this.outputFilePrefix = new File(outputFolder.toFileString(), configuredInputProjectName);
+        this.outputPrefix = Paths.get(CommonPlugin.asLocalURI(outputFolder)
+            .devicePath())
+            .resolve(configuredInputProjectName)
+            .toString();
     }
 
     @Override
@@ -46,15 +55,15 @@ public class PersistenceJob implements IBlackboardInteractingJob<Blackboard<Obje
         Repository repository = (Repository) this.blackboard.getPartition(repositoryKey);
         System system = (System) this.blackboard.getPartition(systemKey);
         ResourceEnvironment resourceEnvironment = (ResourceEnvironment) this.blackboard
-                .getPartition(resourceEnvironmentKey);
+            .getPartition(resourceEnvironmentKey);
         Allocation allocation = (Allocation) this.blackboard.getPartition(allocationKey);
 
         // Make blackboard models persistent by saving them as files
         monitor.subTask("Persisting models");
-        ModelSaver.saveRepository(repository, outputFilePrefix.getPath(), false);
-        ModelSaver.saveSystem(system, outputFilePrefix.getPath(), false);
-        ModelSaver.saveResourceEnvironment(resourceEnvironment, outputFilePrefix.getPath(), false);
-        ModelSaver.saveAllocation(allocation, outputFilePrefix.getPath(), false);
+        ModelSaver.saveRepository(repository, outputPrefix, false);
+        ModelSaver.saveSystem(system, outputPrefix, false);
+        ModelSaver.saveResourceEnvironment(resourceEnvironment, outputPrefix, false);
+        ModelSaver.saveAllocation(allocation, outputPrefix, false);
         monitor.done();
     }
 
